f115350632656ba4ca656b85160f0f9422fd7ef8
jack <jrfisch95@gmail.com>
2023-11-11 17:18:12 -0500

    add solutions for lesson 6-7

--- 06-intro-6/mpow.glsl
+++ 06-intro-6/mpow.glsl
@@ -1,4 +1,6 @@
 mat2 matrixPower(highp mat2 m, int n) {
+
+  const int MAX_ITERATIONS = 100;
   
   //Raise the matrix m to nth power
 
@@ -6,8 +8,16 @@ mat2 matrixPower(highp mat2 m, int n) {
   //
   //  matrixPower(m, 2) = m * m
   //
+  highp mat2 s = m;
 
-  return mat2(1.0);  
+  for(int i = 2; i < MAX_ITERATIONS; i++){
+    if(i > n) {
+      break;
+    }
+    s = m * s;
+  }
+
+  return s;
 }
 
 //Do not change this line or the name of the above function
--- 07-frag-1/fragment.glsl
+++ 07-frag-1/fragment.glsl
@@ -5,11 +5,10 @@ precision highp float;
 
 void main() {
 
-  //TODO: Replace this with a function that draws a circle at (256.5,256.5) with radius 128
-  
-  if(gl_FragCoord.y > 256.0) {
+  if(sqrt((gl_FragCoord.x - 256.5) * (gl_FragCoord.x - 256.5) + (gl_FragCoord.y - 256.5) * (gl_FragCoord.y - 256.5)) < 128.0) {
     gl_FragColor = CIRCLE_COLOR;
   } else {
     gl_FragColor = OUTSIDE_COLOR;
   }
+
 }
