c77af99348e5f1026b90d04c94aeab8610ff011d
jack <jrfisch95@gmail.com>
2023-11-14 19:01:08 -0500

    add partial solution for lesson 10

--- 10-vert-1/vertex.glsl
+++ 10-vert-1/vertex.glsl
@@ -6,7 +6,12 @@ attribute vec2 position;
 
 void main() {
 
-  //TODO: rotate position by theta radians about the origin
+  // vec2 rotated = vec2( (position.x + cos(theta)) / position.y, (position.y - sin(theta)) / position.y) ;
+  // gl_Position = vec4(rotated, 0.0, 1.0);
 
-  gl_Position = vec4(position, 0, 1.0);
+  mat2 clockwiseRotationMatrix = mat2(cos(theta), -sin(theta), sin(theta), cos(theta));
+  mat2 counterclockwiseRotationMatrix = mat2(-sin(theta), cos(theta), cos(theta), sin(theta));
+  
+
+  gl_Position = vec4(clockwiseRotationMatrix * position, 0.0, 1.0);
 }
