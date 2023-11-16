1c85df16e64091f3d6d72aace9377b5e264c289d
jack <jrfisch95@gmail.com>
2023-11-15 19:05:56 -0500

    add full solution for lesson 10

--- 10-vert-1/vertex.glsl
+++ 10-vert-1/vertex.glsl
@@ -6,12 +6,9 @@ attribute vec2 position;
 
 void main() {
 
-  // vec2 rotated = vec2( (position.x + cos(theta)) / position.y, (position.y - sin(theta)) / position.y) ;
-  // gl_Position = vec4(rotated, 0.0, 1.0);
-
   mat2 clockwiseRotationMatrix = mat2(cos(theta), -sin(theta), sin(theta), cos(theta));
-  mat2 counterclockwiseRotationMatrix = mat2(-sin(theta), cos(theta), cos(theta), sin(theta));
+  mat2 counterclockwiseRotationMatrix = mat2(cos(theta), sin(theta), -sin(theta), cos(theta));
   
 
-  gl_Position = vec4(clockwiseRotationMatrix * position, 0.0, 1.0);
+  gl_Position = vec4(counterclockwiseRotationMatrix * position, 0.0, 1.0);
 }
