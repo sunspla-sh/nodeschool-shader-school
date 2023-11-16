c9a2159ba3930da5be2c2999dbda60b816a75bf3
jack <jrfisch95@gmail.com>
2023-11-16 18:18:13 -0500

    add solutions for lessons 11-12

--- 12-geom-1/transforms.glsl
+++ 12-geom-1/transforms.glsl
@@ -7,6 +7,7 @@ uniform mat4 model, view, projection;
 void main() {
 
   //TODO: Apply the model-view-projection matrix to `position`
+  vec4 clip = projection * view * model * vec4(position, 1);
 
-  gl_Position = vec4(position, 1);
+  gl_Position = clip;
 }
