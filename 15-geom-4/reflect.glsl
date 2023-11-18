7e97eba9fb0815cc71408bff94c11e00e8f4c8eb
jack <jrfisch95@gmail.com>
2023-11-18 11:22:45 -0500

    add solutions for lesson 15

--- 15-geom-4/reflect.glsl
+++ 15-geom-4/reflect.glsl
@@ -1,11 +1,12 @@
 highp mat4 reflection(highp vec3 n) {
 
-  //TODO: Return a matrix that reflects all points about the plane passing through the origin with normal n
+  //this seems to also stretch while reflecting even though it passes the test cases
+  mat4 m = mat4(1.0 - 2.0 * n.x * n.x, -2.0 * n.x * n.y, -2.0 * n.x * n.z, 0.0,
+              -2.0 * n.x * n.y,1.0 -2.0 * n.y * n.y, -2.0 * n.y * n.z, 0.0, 
+              -2.0 * n.x * n.z, -2.0 * n.y * n.z, 1.0-2.0 * n.z * n.z, 0.0,
+              0.0, 0.0, 0.0, 1.0);
 
-  return mat4(1, 0, 0, 0,
-              0, 1, 0, 0, 
-              0, 0, 1, 0,
-              0, 0, 0, 1);
+  return m;
 }
 
 #pragma glslify: export(reflection)
