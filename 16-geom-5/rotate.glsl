59104e434d657dcf7c8bc9a931dfd5afc0418740
jack <jrfisch95@gmail.com>
2023-11-19 21:34:16 -0500

    begin lesson 16

--- 16-geom-5/rotate.glsl
+++ 16-geom-5/rotate.glsl
@@ -1,11 +1,9 @@
 highp mat4 rotation(highp vec3 n, highp float theta) {
 
   //TODO: Using Rodrigues' formula, find a matrix which performs a rotation about the axis n by theta radians
+  mat4 m = mat4(1.0);
 
-  return mat4(1, 0, 0, 0,
-              0, 1, 0, 0, 
-              0, 0, 1, 0,
-              0, 0, 0, 1);
+  return m;
 }
 
 #pragma glslify: export(rotation)
